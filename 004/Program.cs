e01247b2e5ed30a782f2d702a38a5aadc16bac25
Aleksandr <pandom@yandex.ru>
2023-01-22 01:00:12 +0300

    004 Палиндром

---
+++ 004/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа проверяет пятизначное число на палиндром. Пример: 12321
+Console.Clear();
+//int number = new Random().Next(10000,99999); Замучаешься повторять программу)
+Console.WriteLine("Введите пятизначное число");
+int number = int.Parse(Console.ReadLine()); // Проще ввести вручную!
+//Console.WriteLine("Дано число " + number);
+int a = number % 10;
+int b = number % 100 / 10;
+//int с = number % 1000 / 100;
+int d = number % 10000 / 1000;
+int e = number % 100000 / 10000;
+if(a == e && b == d)
+{
+    Console.WriteLine("Число " + number + " является палиндромом");
+}
+else
+{
+    Console.WriteLine("Повторите программу");
+}
